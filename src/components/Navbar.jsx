d13ec232dce9e0ddfa3b12aaf8bc8c9fe572f487
suren <surenpal666@gmail.com>
2026-02-22 00:12:59 +0900

    padding at the bottom is added here

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 export default function Navbar() {
   return (
-    <nav className="w-full h-16 bg-gray-50 fixed top-0 left-0 z-50 shadow-lg backdrop-blur-md">
+    <nav className="w-full h-20 bg-black-600 fixed top-0 left-0 z-50 shadow-lg backdrop-blur-md">
       <div className="max-w-6xl mx-auto h-full px-6 flex justify-between items-center">
-        <h1 className="text-3xl font-extrabold text-pink-600 text-shadow-md">
+        <h1 className="text-3xl font-extrabold text-shadow-md">
           SP
         </h1>
 
-        <div className="space-x-8 text-pink-700 font-medium">
-          <a href="#about" className="hover:text-pink-600 transition-colors">About</a>
-          <a href="#skills" className="hover:text-pink-600 transition-colors">Skills</a>
-          <a href="#projects" className="hover:text-pink-600 transition-colors">Projects</a>
-          <a href="#contact" className="hover:text-pink-600 transition-colors">Contact</a>
+        <div className="space-x-8 text-gray-900 font-bold">
+          <a href="#about" className="hover:text-gray-600 transition-colors">About</a>
+          <a href="#skills" className="hover:text-gray-600 transition-colors">Skills</a>
+          <a href="#projects" className="hover:text-gray-600 transition-colors">Projects</a>
+          <a href="#contact" className="hover:text-gray-600 transition-colors">Contact</a>
         </div>
       </div>
     </nav>
