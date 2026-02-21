d9252b18fdcf85a0e33eec11154038f63c4e1bee
suren <surenpal666@gmail.com>
2026-02-22 01:00:22 +0900

    resolving for the bg-abstracts

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,19 +4,22 @@ export default function About() {
   return (
     <section
       id="about"
-      className="w-full py-24 px-6 text-center bg-gray-50"
+      className=" relative w-full py-24 px-6 text-center bg-[url('/bg-img-all.jpg')] bg-cover bg-center bg-no-repeat"
     >
-      <h2 className="text-4xl font-extrabold text-green-600 mb-6 text-shadow-md">
-        About Me
-      </h2>
+      <div className="absolute inset-0 bg-white/60"></div>
 
-      <p className="text-gray-700 leading-relaxed text-lg mb-10">
-        I am a React developer who loves building learner-friendly resources.
-        I enjoy simplifying complex topics and creating clean,<br /> scalable
-        projects for teaching and real-world use.
-      </p>
+       <div className="bg-white/0 backdrop-blur-sm p-8 rounded-2xl inline-block max-w-3xl mx-auto border-none">
+        <h2 className="text-4xl font-extrabold text-green-600 mb-6">
+          About Me
+        </h2>
 
+        <p className="text-gray-700 leading-relaxed text-lg mb-10">
+          I am a React developer who loves building learner-friendly resources.
+          I enjoy simplifying complex topics and creating clean,<br /> scalable
+          projects for teaching and real-world use.
+        </p>
       <MyCareer />
+      </div>
     </section>
   );
 }
