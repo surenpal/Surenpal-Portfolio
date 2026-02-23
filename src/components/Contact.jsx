26a4263271d1a99237aba6023c7967960d1512cd
suren <surenpal666@gmail.com>
2026-02-23 23:30:16 +0900

    yosh the image is added here

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
 <section
   id="contact"
-  className="relative w-full py-24 px-6 text-center bg-[url('/your-image.jpg')] bg-cover bg-center"
+  className="relative w-full py-24 px-6 text-center bg-[url('/bg-contact.jpg')] bg-cover bg-center"
 >
   <div className="absolute inset-0 bg-black/40"></div>
 
