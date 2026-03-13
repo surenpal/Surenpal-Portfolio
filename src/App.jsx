231682b3a184a658d1d72cfc09c3eef13f7a4b51
suren <surenpal666@gmail.com>
2026-03-13 20:08:48 +0900

    logic for the dark is added to using the useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,16 @@ export default function App() {
 
   const [theme, setTheme] = useState("light");
 
+  useEffect(() => 
+  {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    }
+    else{
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
   return (
     <div className="bg-gray-400 text-gray-900 min-h-screen bg-cover bg-center bg-fixed bg-no-repeat bg-[url('/bg-all.jpg')]">
       <Navbar theme={theme} setTheme={setTheme} />
