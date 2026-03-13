f44f2663f19fc55a062218cfd29b2a0f3c72fc08
suren <surenpal666@gmail.com>
2026-03-13 20:02:27 +0900

    theme and setTheme is stored in useState,thesse are the const/variables

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 export default function App() {
+
+  const [theme, setTheme] = useState("light");
+
   return (
     <div className="bg-gray-400 text-gray-900 min-h-screen bg-cover bg-center bg-fixed bg-no-repeat bg-[url('/bg-all.jpg')]">
       <Navbar />
